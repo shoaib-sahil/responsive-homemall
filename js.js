58ebc186d77b07ac87edba0258108872b7657e8d
shoaib-sahil <103852061+shoaib-sahil@users.noreply.github.com>
2022-05-31 23:21:05 -0700

    Add files via upload
    responsive homemall web

---
+++ js.js
@@ -0,0 +1,20 @@
+let searchForm = document.querySelector('.search_form');
+
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+
+
+
+let shoppingCart = document.querySelector('.shopping_cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+}
+
+
+
+
+
